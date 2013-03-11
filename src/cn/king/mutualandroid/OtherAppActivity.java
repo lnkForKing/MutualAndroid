235b456d1ff1cc0ddd6c676f74c9552fca50962b
Administrator <Administrator@PC-201303091219>
2013-03-11 23:35:37 +0800

    被别的应用调用activity

--- src/cn/king/mutualandroid/OtherAppActivity.java
+++ src/cn/king/mutualandroid/OtherAppActivity.java
@@ -3,30 +3,23 @@ package cn.king.mutualandroid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-	private static final String TAG = "MainActivity";
+public class OtherAppActivity extends Activity {
+	private static final String TAG = "";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 		boolean fromOther = this.getIntent().getBooleanExtra("other", false);
 		Log.i(TAG,"fromOther:" + fromOther);
 		if(fromOther){
 			String hello = this.getIntent().getStringExtra("hello");
 			TextView helloTxt = (TextView)this.findViewById(R.id.hello);
-			helloTxt.setText("主界面，来自应用："+hello);
+			helloTxt.setText("其它界面，来自应用："+hello);
 		}
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+	
 }
